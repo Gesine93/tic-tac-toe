dc92b4b8fc89d9fbf87027f5e54a7c63addd0337
Gesine Fengler <gesine.fengler@gmx.de>
2024-08-04 22:53:38 +0200

    Changed style and some minor things in script.js: Changed collors and font in CSS. Added display of names in Script.

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function getPlayer(player_x, player_o) {
     return player;
 }
 
-function checkWinner(board, currentPlayer) {
+function checkWinner(board) {
     if ((board[0] !== "" && board[0] === board[1] && board[1] === board[2])
         || (board[3] !== "" && board[3] === board[4] && board[4] === board[5])
         || (board[6] !== "" && board[6] === board[7] && board[7] === board[8])
@@ -47,15 +47,14 @@ function checkWinner(board, currentPlayer) {
     }
 }
 
-function playGame(play) {
+function playGame(play, player_x, player_o) {
     let form = document.querySelector(".form");
-    const player_x = createPlayer("x", "Player One");
-    const player_o = createPlayer("o", "Player Two");
     const board = gameboard.board;
     const round = displayRound();
     let player = getPlayer(player_x, player_o);
     let fields = document.querySelectorAll("td");
     let result = document.querySelector("p");
+    result.textContent = `It's ${player.name}'s turn!`;
 
     function placeSign(event) {
         const field = event.target;
@@ -65,13 +64,15 @@ function playGame(play) {
             player.giveRound();
             round.giveRound();
             if (checkWinner(board, player)) {
+                endGame();
                 result.textContent = `${player.name} wins!`;
-                endGame();
             } else if (round.getRound() > 9) {
-                result.textContent = "It's a draw!";
                 endGame();
+                result.textContent = "It's a draw!";
+            } else {
+                player = getPlayer(player_x, player_o);
+                result.textContent = `It's ${player.name}'s turn!`;
             }
-            player = getPlayer(player_x, player_o);
         }
     }
 
@@ -79,7 +80,6 @@ function playGame(play) {
         fields.forEach(field => field.removeEventListener('click', placeSign));
         play.textContent = "Restart Game";
         form.classList.toggle("inactive");
-        result.classList.toggle("inactive");
         play.addEventListener("click", restartGame, { once: true });
     }
 
@@ -89,7 +89,22 @@ function playGame(play) {
             board[i] = "";
         }
         round.reset();
-        playGame(play);
+        form.classList.toggle("inactive");
+        let one = document.querySelector('input[name="one"]').value;
+        let two = document.querySelector('input[name="two"]').value;
+        if (one!=="") {
+            player_x = createPlayer("x", one);
+        } else {
+            player_x = createPlayer("x", "Player One");
+        }
+        if (one!=="") {
+            player_o = createPlayer("o", two);
+        } else {
+            player_o = createPlayer("o", "Player Two");
+        };
+        form.classList.toggle("inactive");
+
+        playGame(play, player_x, player_o);
     }
 
     fields.forEach(field => {
@@ -101,13 +116,26 @@ function playGame(play) {
 document.addEventListener("DOMContentLoaded", () => {
     let play = document.querySelector("button");
     let form = document.querySelector(".form");
-    let result = document.querySelector("p");
+    let player_x;
+    let player_o;
     play.textContent = "Start Game";
-    play.addEventListener("click", () => {
+    play.addEventListener("click", (e) => {
+        e.preventDefault();
         form.classList.toggle("inactive");
-        result.classList.toggle("inactive");
-        playGame(play);
-    })
+        let one = document.querySelector('input[name="one"]').value;
+        let two = document.querySelector('input[name="two"]').value;
+        if (one!=="") {
+            player_x = createPlayer("x", one);
+        } else {
+            player_x = createPlayer("x", "Player One");
+        }
+        if (one!=="") {
+            player_o = createPlayer("o", two);
+        } else {
+            player_o = createPlayer("o", "Player Two");
+        }
+        playGame(play, player_x, player_o);
+    });
 });
 
 
